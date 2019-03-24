6bae81bf9297f517b80769318ad6f3c880309708
apidcloud <luisapidcloud@hotmail.com>
2019-03-23 18:34:20 -0600

    chore: working simple example with UMD

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: "TS",
+    // export itself to UMD format
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    // fix for https://github.com/webpack/webpack/issues/6525
+    globalObject: `(typeof self !== 'undefined' ? self : this)`
     },
   mode: 'development',
   optimization: {
